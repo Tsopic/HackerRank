5bbfcf366727de0a75a28236a341ce58857e76b6
Martin Kask <Tsopic@users.noreply.github.com>
2019-08-19 13:02:45 +0300

    Update day-15-Linked-List.js
    beautify

--- day-15-Linked-List.js
+++ day-15-Linked-List.js
@@ -24,25 +24,25 @@ function Solution(){
     class Node {
         constructor(data, next = null) {
             this.data = data,
-                this.next = next
+            this.next = next
         }
     }
 
 	this.insert=function(head,data){
-        let newNode = new Node(data);
-        // When head = null i.e. the list is empty, then head itself will point to the newNode.
-        if (!this.head) {
-            this.head = newNode;
-            return this.head;
-        }
-        // Else, traverse the list to find the tail (the tail node will initially be pointing at null), and update the tail's next pointer.
-        let tail = this.head;
-        while (tail.next !== null) {
-            tail = tail.next;
-        }
-        tail.next = newNode;
-        return this.head;
-    };
+		let newNode = new Node(data);
+		// When head = null i.e. the list is empty, then head itself will point to the newNode.
+		if (!this.head) {
+		    this.head = newNode;
+		    return this.head;
+		}
+		// Else, traverse the list to find the tail (the tail node will initially be pointing at null), and update the tail's next pointer.
+		let tail = this.head;
+		while (tail.next !== null) {
+		    tail = tail.next;
+		}
+		tail.next = newNode;
+		return this.head;
+    	};
 
 	this.display=function(head){
         var start=head;
